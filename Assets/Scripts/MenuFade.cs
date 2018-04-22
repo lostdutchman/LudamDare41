fc4177363caf26502e0c2865beb83dfd1c0fcce0
JeremyT <waddledoocanto@gmail.com>
2018-04-22 14:00:08 -0700

    Splash Screen Fade

---
+++ Assets/Scripts/MenuFade.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuFade : MonoBehaviour {
+
+	public Image Fade;
+	public GameObject FadeObj;
+
+	// Use this for initialization
+	void Start () {
+		Fade.CrossFadeAlpha (0f, 3.5f, false);
+		StartCoroutine(FadeWait ());
+	}
+
+	IEnumerator FadeWait() {
+		yield return new WaitForSeconds (3.5f);
+		FadeObj.SetActive (false);
+	}
+}
