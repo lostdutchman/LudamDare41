4d33e4fafcf79096738f2b7d64bb306084eae35b
Sam <Sam@MAGICKINGDOM>
2018-04-21 20:13:51 -0700

    main menu and menu hook ups

---
+++ Assets/ButtonFX.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonFX : MonoBehaviour {
+
+	public AudioSource myFX;
+	public AudioClip hoverFX;
+	public AudioClip clickFX;
+
+	public void HoverSound()
+	{
+		myFX.PlayOneShot (hoverFX);
+	}
+	public void ClickSound()
+	{
+		myFX.PlayOneShot (clickFX);
+	}
+}
