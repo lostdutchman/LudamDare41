aa52a5a9b0b38fa2a6d338e0473649864560ee41
JeremyT <waddledoocanto@gmail.com>
2018-04-22 12:57:03 -0700

    End scene script in place

---
+++ Assets/Scripts/YouWin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YouWin : MonoBehaviour {
+
+	public GameObject M1, M2, M3, M4, M5, Victory;
+	public float Pause = .5f;
+
+	void Start(){
+		Merit();
+	}
+
+	IEnumerator Merit() {
+		M1.SetActive (true);
+		yield return new WaitForSeconds (Pause);
+		M2.SetActive (true);
+		yield return new WaitForSeconds (Pause);
+		M3.SetActive (true);
+		yield return new WaitForSeconds (Pause);
+		M4.SetActive (true);
+		yield return new WaitForSeconds (Pause);
+		M5.SetActive (true);
+		yield return new WaitForSeconds (Pause * 2);
+		Victory.SetActive (true);
+	}
+}
