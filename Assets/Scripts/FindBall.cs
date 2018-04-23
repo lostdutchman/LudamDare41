54e9d387d8e8c91aa73e6ef337e3dc85c85367a3
JeremyT <waddledoocanto@gmail.com>
2018-04-22 19:41:00 -0700

    Ball find script completo

--- Assets/Scripts/FindBall.cs
+++ Assets/Scripts/FindBall.cs
@@ -14,9 +14,17 @@ public class FindBall : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Tab)) {
-			Particals.enableEmission = true;
-		} else {
-			Particals.enableEmission = false;
-		}
+			StartCoroutine(LightUp());
+		} 
+	}
+	
+	IEnumerator LightUp() {
+		Particals.Emit(50);
+		yield return new WaitForSeconds (.3f);
+		Particals.Emit(50);
+		yield return new WaitForSeconds (.3f);
+		Particals.Emit(50);
+		yield return new WaitForSeconds (.5f);
+		Particals.Emit(50);
 	}
 }
