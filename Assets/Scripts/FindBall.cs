3734bb9676c037906873c81700465232b6579cf1
JeremyT <waddledoocanto@gmail.com>
2018-04-22 20:12:57 -0700

    Fixed Ball script

--- Assets/Scripts/FindBall.cs
+++ Assets/Scripts/FindBall.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class FindBall : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	private Time TimeSinceLastHit;
+	private ParticleSystem Particals;
+	
+	void Start()
+	{
+		Particals = GetComponent<ParticleSystem>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			StartCoroutine(LightUp());
+		} 
+	}
 	
+	IEnumerator LightUp() {
+		Particals.Emit(50);
+		yield return new WaitForSeconds (.3f);
+		Particals.Emit(50);
+		yield return new WaitForSeconds (.3f);
+		Particals.Emit(50);
+		yield return new WaitForSeconds (.5f);
+		Particals.Emit(50);
 	}
 }
