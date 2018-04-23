6fefc13afc6a1ed87c8e233a8545b0a20ce67463
JeremyT <waddledoocanto@gmail.com>
2018-04-22 19:21:08 -0700

    Particals?

--- Assets/Scripts/FindBall.cs
+++ Assets/Scripts/FindBall.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class FindBall : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private Time TimeSinceLastHit;
+	private ParticleSystem Particals;
+
+	void Start()
+	{
+		Particals = GetComponent<ParticleSystem>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			Particals.enableEmission = true;
+		} else {
+			Particals.enableEmission = false;
+		}
 	}
 }
